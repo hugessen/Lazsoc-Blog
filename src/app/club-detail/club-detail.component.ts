26812239756ae0ef551c02976999b754fbb058be
Mastz64 <hamzah.uzair@hotmail.com>
2017-10-18 21:18:26 -0400

    Added Photos and fixed spacing of images

--- src/app/club-detail/club-detail.component.ts
+++ src/app/club-detail/club-detail.component.ts
@@ -40,9 +40,9 @@ export class ClubDetailComponent implements OnInit {
           title: "President & Internal",
           id: "One",
           team: [
-            { name: "Angelo Fousteris", title: "President", image: "assets/img/thumbnails/LazSoc.png" }, 
-            { name: "Kara McDowell", title: "Internal Director", image: "assets/img/thumbnails/LazSoc.png" },
-            { name: "Manpreet Brar", title: "International Ambassador", image: "assets/img/thumbnails/LazSoc.png" }
+            { name: "Angelo Fousteris", title: "President", image: "assets/img/Head Shots/Angelo1.jpg" }, 
+            { name: "Kara McDowell", title: "Internal Director", image: "assets/img/Head Shots/Kara1.jpg" },
+            { name: "Manpreet Brar", title: "International Ambassador", image: "assets/img/Head Shots/Manpreet.jpg" }
           ]
         },
         {
@@ -51,25 +51,25 @@ export class ClubDetailComponent implements OnInit {
           team: [
             { name: "Rebecca Knight", title: "VP of Marketing & Communications", image: "assets/img/thumbnails/LazSoc.png" },
             { name: "Rachel Guerreiro", title: "Director", image: "assets/img/thumbnails/LazSoc.png" },
-            { name: "Mahad Aamir", title: "Design Director", image: "assets/img/thumbnails/LazSoc.png" },
-            { name: "Vanessa Morsink", title: "Public Relations", image: "assets/img/thumbnails/LazSoc.png" },
+            { name: "Mahad Aamir", title: "Design Director", image: "assets/img/Head Shots/Mahad.jpg" },
+            { name: "Vanessa Morsink", title: "Public Relations", image: "assets/img/Head Shots/Vanessa.jpg" },
             { name: "Parry Rickers", title: "Social Media", image: "assets/img/thumbnails/LazSoc.png" },
-            { name: "Dayna Nairn", title: "Design Manager", image: "assets/img/thumbnails/LazSoc.png" },
-            { name: "Sam Weinberger", title: "Merch & Apparel", image: "assets/img/thumbnails/LazSoc.png" },
-            { name: "Keerthi Ravichandran", title: "Multimedia", image: "assets/img/thumbnails/LazSoc.png" },
-            { name: "Heba Hussein", title: "Photographer", image: "assets/img/thumbnails/LazSoc.png" }
+            { name: "Dayna Nairn", title: "Design Manager", image: "assets/img/Head Shots/Dayna.jpg" },
+            { name: "Sam Weinberger", title: "Merch & Apparel", image: "assets/img/Head Shots/SamW.jpg" },
+            { name: "Keerthi Ravichandran", title: "Multimedia", image: "assets/img/Head Shots/Keerthi.jpg" },
+            { name: "Heba Hussein", title: "Photographer", image: "assets/img/Head Shots/Heba.jpg" }
           ]
         },
         {
           title: "Corporate Relations",
           id: "Three",
           team: [
-            { name: "Michelle Stratulat", title: "VP of Corporate Relations", image: "assets/img/thumbnails/LazSoc.png" },
+            { name: "Michelle Stratulat", title: "VP of Corporate Relations", image: "assets/img/Head Shots/Michelle.jpg" },
             { name: "Madison MacLead", title: "Manager-Internal", image: "assets/img/thumbnails/LazSoc.png" },
-            { name: "Justin Dube", title: "Manager-Internal", image: "assets/img/thumbnails/LazSoc.png" },
-            { name: "Xavier Prosper", title: "Manager-Internal", image: "assets/img/thumbnails/LazSoc.png" },
-            { name: "Jack Heinzl", title: "Manager-Internal", image: "assets/img/thumbnails/LazSoc.png" },
-            { name: "Hannah Klien", title: "Specialistr", image: "assets/img/thumbnails/LazSoc.png" },
+            { name: "Justin Dube", title: "Manager-Internal", image: "assets/img/Head Shots/JustinD.jpg" },
+            { name: "Xavier Prosper", title: "Manager-Internal", image: "assets/img/Head Shots/Xavier.jpg" },
+            { name: "Jack Heinzl", title: "Manager-Internal", image: "assets/img/Head Shots/JackH.jpg" },
+            { name: "Hannah Klien", title: "Specialistr", image: "assets/img/Head Shots/Hannah.jpg" },
             { name: "Justin Sung", title: "C & C Outreach", image: "assets/img/thumbnails/LazSoc.png" }
           ]
         },
@@ -77,44 +77,44 @@ export class ClubDetailComponent implements OnInit {
           title: "Finance",
           id: "Four",
           team: [
-            { name: "Raluca Truta", title: "VP of Finance", image: "assets/img/thumbnails/LazSoc.png" },
-            { name: "Diane Oliver", title: "Internal Director", image: "assets/img/thumbnails/LazSoc.png" },
-            { name: "Soo-Ah Youn", title: "Internal Director", image: "assets/img/thumbnails/LazSoc.png" },
-            { name: "Shruti Uppal", title: "Internal Director", image: "assets/img/thumbnails/LazSoc.png" },
-            { name: "Jacob Krohnert", title: "Internal Director", image: "assets/img/thumbnails/LazSoc.png" },
-            { name: "Jack Fischer", title: "Director of C & C", image: "assets/img/thumbnails/LazSoc.png" },
-            { name: "Sam Hall", title: "Financial Assistant", image: "assets/img/thumbnails/LazSoc.png" } 
+            { name: "Raluca Truta", title: "VP of Finance", image: "assets/img/Head Shots/Raluca.jpg" },
+            { name: "Diane Oliver", title: "Internal Director", image: "assets/img/Head Shots/Diane.jpg" },
+            { name: "Soo-Ah Youn", title: "Internal Director", image: "assets/img/Head Shots/Soo-ah.jpg" },
+            { name: "Shruti Uppal", title: "Internal Director", image: "assets/img/Head Shots/Shruti.jpg" },
+            { name: "Jacob Krohnert", title: "Internal Director", image: "assets/img/Head Shots/JacobK.jpg" },
+            { name: "Jack Fischer", title: "Director of C & C", image: "assets/img/Head Shots/JackF.jpg" },
+            { name: "Sam Hall", title: "Financial Assistant", image: "assets/img/Head Shots/SamH.jpg" } 
           ]
         },
         {
           title: "Clubs & Associations",
           id: "Five",
           team: [
-            { name: "Emily Fraser", title: "VP of Clubs & Associations", image: "assets/img/thumbnails/LazSoc.png" },
+            { name: "Emily Fraser", title: "VP of Clubs & Associations", image: "assets/img/Head Shots/Emily1.jpg" },
             { name: "Yohan Billmoria", title: "Club Specialist", image: "assets/img/thumbnails/LazSoc.png" },
-            { name: "James Varley", title: "Club Specialist", image: "assets/img/thumbnails/LazSoc.png" },
-            { name: "Theo Will-Dryden", title: "Director of Philanthrophy", image: "assets/img/thumbnails/LazSoc.png" },
-            { name: "Sarah Arnott", title: "Philanthrophy Manager", image: "assets/img/thumbnails/LazSoc.png" },
-            { name: "Kristen Marshall", title: "Philanthrophy Manager", image: "assets/img/thumbnails/LazSoc.png" }
+            { name: "James Varley", title: "Club Specialist", image: "assets/img/Head Shots/JamieV.jpg" },
+            { name: "Theo Will-Dryden", title: "Director of Philanthrophy", image: "assets/img/Head Shots/Theo.jpg" },
+            { name: "Sarah Arnott", title: "Philanthrophy Manager", image: "assets/img/Head Shots/Sara1.jpg" },
+            { name: "Kristen Marshall", title: "Philanthrophy Manager", image: "assets/img/Head Shots/Kristen2.jpg" }
           ]
         },
         {
           title: "Student Engagement",
           id: "Six",
           team: [
-            { name: "Marnie McCormac", title: "VP of Student Engagement", image: "assets/img/thumbnails/LazSoc.png" },
-            { name: "Yohan Billmoria", title: "Director of Social Events", image: "assets/img/thumbnails/LazSoc.png" },
-            { name: "James Varley", title: "Director of Academic Events", image: "assets/img/thumbnails/LazSoc.png" },
-            { name: "Theo Will-Dryden", title: "Events Manager", image: "assets/img/thumbnails/LazSoc.png" },
-            { name: "Sarah Arnott", title: "Events Manager", image: "assets/img/thumbnails/LazSoc.png" }
+            { name: "Marnie McCormac", title: "VP of Student Engagement", image: "assets/img/Head Shots/Marnie.jpg" },
+            { name: "Olivia Bowe", title: "Director of Social Events", image: "assets/img/Head Shots/OliviaB.jpg" },
+            { name: "Alena Guits", title: "Director of Academic Events", image: "assets/img/Head Shots/Alena.jpg" },
+            { name: "Rebecca Lay", title: "Events Manager", image: "assets/img/Head Shots/Rebecca.jpg" },
+            { name: "Dakota Van Halteren", title: "Events Manager", image: "assets/img/Head Shots/Dakota.jpg" }
           ] 
         },
         {
           title: "Technology Development",
           id: "Five",
           team: [
-            { name: "Richard Hugessen", title: "VP of Technology Development", image: "assets/img/thumbnails/LazSoc.png" },
-            { name: "Riyaz Shaikh", title: "Web Developer", image: "assets/img/thumbnails/LazSoc.png" },
+            { name: "Richard Hugessen", title: "VP of Technology Development", image: "assets/img/Head Shots/Richard.jpg" },
+            { name: "Riyaz Shaikh", title: "Web Developer", image: "assets/img/Head Shots/Riyaz.jpg" },
             { name: "Owen Van Valkenberg", title: "Mobile App Developer", image: "assets/img/thumbnails/LazSoc.png" },
             { name: "Hamzah Zia", title: "Web Developer", image: "assets/img/thumbnails/LazSoc.png" },
           ]
