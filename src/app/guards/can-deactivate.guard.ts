c978496a86a381fe8d3f0e95ec2734470b57658c
Richard Hugessen <richard@hugessen.ca>
2018-01-30 18:28:37 -0500

    Addendum to email validation patch

---
+++ src/app/guards/can-deactivate.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable }     from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, CanDeactivate, Router } from "@angular/router";
+import { AuthService } from '../services/auth.service';
+import { UpdateComponent } from '../update/update.component';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class CanDeactivateUpdate implements CanDeactivate<UpdateComponent> {
+  constructor(private authService:AuthService) {}
+ 
+  canDeactivate(
+    component: UpdateComponent,
+    currentRoute: ActivatedRouteSnapshot,
+    currentState: RouterStateSnapshot,
+    nextState: RouterStateSnapshot
+  ): Observable<boolean>|Promise<boolean>|boolean {
+  	return component.canDeactivate()
+  }
+}
