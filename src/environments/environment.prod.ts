700f621ff0f1505eba731510f54e2346f9eb9b82
Richard Hugessen <richard@hugessen.ca>
2018-08-25 12:55:38 -0400

    Update to environment.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,10 @@
 export const environment = {
-  production: true,
+  production: false,
   token_auth_config: {
     apiBase: 'http://localhost:3000/api'
   },
   aws_access: 'AKIAIGEQRRLJR7KOXTAA',
-  aws_secret: `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`
+  aws_secret: `+ae2krSo/AZufJAzmWYMaG7YYBzKqVHhYPlOz/30`,
+  SMILE_PUBLIC: `channel_M5x6LKKhukrmV3OWEMAOJddt`,
+	SMILE_SECRET: `int_vGzV9qMuVdEucMF1UY5y`
 };
